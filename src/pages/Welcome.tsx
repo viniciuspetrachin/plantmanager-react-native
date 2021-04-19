04b0e3845a1878b5aeeece50bb94ea367ca17fff
Vinicius Petrachin <vr.petrachin@gmail.com>
2021-04-19 14:15:25 -0300

    Added SplashScreen, WelcomeScreen

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export function Welcome() {
+  return (
+    <View>
+      <Text>
+        Welcome
+      </Text>
+    </View>
+  )
+}
