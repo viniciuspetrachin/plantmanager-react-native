672dd74e820c6077d2447ce883cccd7c134c8a09
Vinicius Petrachin <vr.petrachin@gmail.com>
2021-04-22 13:39:30 -0300

    Get name from AsyncStorage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,50 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  Image,
-  StyleSheet,
-  Text,
-  View
+   Image,
+   StyleSheet,
+   Text,
+   View
 } from 'react-native';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
 export function Header() {
-  return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.greeting}>Ola</Text>
-        <Text style={styles.userName}>Vinicius</Text>
+
+   const [userName, setUserName] = useState<string>()
+
+   useEffect(() => {
+      loadUserName()
+   }, [])
+
+   async function loadUserName(){
+      const name = await AsyncStorage.getItem('@plantmanager:user')
+      setUserName(name || '')
+   }
+
+   return (
+      <View style={styles.container}>
+         <View>
+            <Text style={styles.greeting}>Ola</Text>
+            <Text style={styles.userName}>{userName}</Text>
+         </View>
+         <Image
+            style={styles.profileImage}
+            source={require('../assets/profile.jpg')}
+         />
       </View>
-      <Image
-      style={styles.profileImage}
-        source={require('../assets/profile.jpg')}
-      />
-    </View>
-  );
+   );
 }
 const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingVertical: 20
-  },
-  profileImage: {
-    height: 70,
-    width: 70,
-    borderRadius: 40
-  },
-  greeting: {
-    fontSize: 32,
-    color: colors.heading,
-    fontFamily: fonts.text
-  },
-  userName: {
-    fontSize: 32,
-    color: colors.heading,
-    fontFamily: fonts.heading,
-    lineHeight: 40
-  }
+   container: {
+      width: '100%',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      paddingVertical: 20
+   },
+   profileImage: {
+      height: 70,
+      width: 70,
+      borderRadius: 40
+   },
+   greeting: {
+      fontSize: 32,
+      color: colors.heading,
+      fontFamily: fonts.text
+   },
+   userName: {
+      fontSize: 32,
+      color: colors.heading,
+      fontFamily: fonts.heading,
+      lineHeight: 40
+   }
 })
